f9d7f8dc12ccba4d8e1fb56520d4cbd17888afac
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-02-17 23:12:44 +0200

    (5 click event)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,38 @@
 import Posts from './posts/Posts'
 const Home = () => {
-  const title="HOME";
-  const styles = { color:"red", backgroundColor:"black" };
+
+  // const handleClick = (e) =>{
+  //   console.log("Mohammed");
+  //   console.log(e);
+
+  // const handleClick = (name) =>{
+  //   console.log(name);
+    // console.log(e);
+
+    let name = "Ali"
+    const handleClick = () =>{
+      name="Mohammed_Azzam";
+      console.log(name);
+  };
   return (
     <div className="home">
-      <h1 style={styles}>{title}</h1>
+      {/* <button className="btn" onClick={handleClick}> */}
+      
+
+      {/* <button 
+      className="btn"
+      onClick={() =>{
+        return handleClick("Azzam")
+      }}>
+
+        Click Me
+      </button> */}
+
+
+<button className="btn"onClick={handleClick}>
+        Click Me
+      </button>
+      <h1>{name}</h1>
       <Posts />
     </div>
   );
