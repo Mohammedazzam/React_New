def41ed6435ca3c7744278c29c570751289d45c5
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-02-18 22:15:36 +0200

    (6 useState Hook )

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,18 @@
-import Posts from './posts/Posts'
+import {useState} from "react";
+import Posts from './posts/Posts';
 const Home = () => {
+    // let name = "Ali"
 
-  // const handleClick = (e) =>{
-  //   console.log("Mohammed");
-  //   console.log(e);
+     let [name, setName] = useState("Ali")
+     const handleClick = () => {
+      // name="Mohammed_Azzam";
+      setName("Mohammed_Azzam")
+      // console.log(name);
+     };
+     console.log(name);
 
-  // const handleClick = (name) =>{
-  //   console.log(name);
-    // console.log(e);
-
-    let name = "Ali"
-    const handleClick = () =>{
-      name="Mohammed_Azzam";
-      console.log(name);
-  };
   return (
     <div className="home">
-      {/* <button className="btn" onClick={handleClick}> */}
-      
-
-      {/* <button 
-      className="btn"
-      onClick={() =>{
-        return handleClick("Azzam")
-      }}>
-
-        Click Me
-      </button> */}
-
-
 <button className="btn"onClick={handleClick}>
         Click Me
       </button>
