b32f85bdd98eced72f054b06d5f3b005700b11ed
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-02-19 22:40:59 +0200

    (7 List Rendering )

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,9 @@
-import {useState} from "react";
 import Posts from './posts/Posts';
 const Home = () => {
-    // let name = "Ali"
 
-     let [name, setName] = useState("Ali")
-     const handleClick = () => {
-      // name="Mohammed_Azzam";
-      setName("Mohammed_Azzam")
-      // console.log(name);
-     };
-     console.log(name);
 
   return (
     <div className="home">
-<button className="btn"onClick={handleClick}>
-        Click Me
-      </button>
-      <h1>{name}</h1>
       <Posts />
     </div>
   );
